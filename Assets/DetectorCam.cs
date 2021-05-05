78fc62f6b973e379a5ec215e44e2a7cb6735bb39
Yusuf Olokoba <olokobayusuf@gmail.com>
2021-05-04 22:36:06 -0400

    First commit

---
+++ Assets/DetectorCam.cs
@@ -0,0 +1,65 @@
+﻿/* 
+*   Detector Cam
+*   Copyright (c) 2021 Yusuf Olokoba.
+*/
+
+namespace NatSuite.Examples {
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Devices;
+    using ML;
+    using ML.Predictors;
+    using ML.Vision;
+    using ML.Visualizers;
+    using Recorders;
+
+    public class DetectorCam : MonoBehaviour {
+
+        [Header("ML")]
+        public MLModelData modelData;
+
+        [Header("Visualization")]
+        public MLDetectionVisualizer visualizer;
+
+        MLModel model;
+        MLAsyncPredictor<(string, Rect, float)[]> predictor;
+        ICameraDevice cameraDevice;
+        private Texture2D previewTexture;
+
+        async void Start () {
+            // Request camera permissions
+            if (!await MediaDeviceQuery.RequestPermissions<ICameraDevice>()) {
+                Debug.LogError(@"User did not grant camera permissions");
+                return;
+            }
+            // Create async predictor
+            model = modelData.Deserialize();
+            predictor = new TinyYOLOv3Predictor(model, modelData.labels).ToAsync();
+            // Get the default camera device
+            var query = new MediaDeviceQuery(MediaDeviceCriteria.GenericCameraDevice);
+            cameraDevice = query.current as ICameraDevice;
+            // Start the camera preview
+            cameraDevice.previewResolution = (1280, 720);
+            previewTexture = await cameraDevice.StartRunning();
+            // Display the camera preview
+            visualizer.Render(previewTexture);
+        }
+
+        async void Update () {
+            // Check if the predictor is ready
+            if (!predictor.readyForPrediction)
+                return;
+            // Predict
+            var detections = await predictor.Predict(previewTexture);
+            // Visualize
+            var visualizations = new List<(Rect, string)>();
+            foreach (var (label, rect, confidence) in detections) {
+                var visualizationText = $"{label}: {confidence:0.##}";
+                visualizations.Add((rect, visualizationText));
+            }
+            visualizer.Render(previewTexture, visualizations.ToArray());
+        }
+    }
+}
