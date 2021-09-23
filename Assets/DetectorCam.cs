83f3da8f0b1b7be642538e3a9db716937eb4da12
Lanre <lanre@krikey.com>
2021-09-23 16:45:39 -0400

    Add ssd-lite, remove tiny-yolo-v3

--- Assets/DetectorCam.cs
+++ Assets/DetectorCam.cs
@@ -11,18 +11,21 @@ namespace NatSuite.Examples {
     using NatSuite.ML.Features;
     using NatSuite.ML.Vision;
     using NatSuite.ML.Visualizers;
-    using NatSuite.ML.Extensions;
 
     public class DetectorCam : MonoBehaviour {
 
-        [Header("Visualization")]
-        public MLDetectionVisualizer visualizer;
+        [Header(@"NatML Hub")]
+        public string accessKey;
+
+        [Header(@"Visualization")]
+        public SSDLiteVisualizer visualizer;
 
         CameraDevice cameraDevice;
         Texture2D previewTexture;
+        byte[] pixelBuffer;
         MLModelData modelData;
         MLModel model;
-        MLAsyncPredictor<(Rect, string, float)[]> predictor;
+        SSDLitePredictor predictor;
 
         async void Start () {
             // Request camera permissions
@@ -34,36 +37,38 @@ namespace NatSuite.Examples {
             var query = new MediaDeviceQuery(MediaDeviceCriteria.CameraDevice);
             cameraDevice = query.current as CameraDevice;
             // Start the camera preview
-            cameraDevice.previewResolution = (1280, 720);
+            cameraDevice.previewResolution = (640, 480);
             previewTexture = await cameraDevice.StartRunning();
+            pixelBuffer = previewTexture.GetRawTextureData<byte>().ToArray();
             // Display the camera preview
             visualizer.Render(previewTexture);
-            // Fetch the TinyYOLO detector
+            // Fetch the model data
             Debug.Log("Fetching model from NatML Hub");
-            modelData = await MLModelData.FromHub("@natsuite/tiny-yolo-v3");
+            modelData = await MLModelData.FromHub("@natsuite/ssd-lite", accessKey);
+            // Deserialize the model
             model = modelData.Deserialize();
-            predictor = new TinyYOLOv3Predictor(model, modelData.labels).ToAsync();
+            // Create the SSD Lite predictor
+            predictor = new SSDLitePredictor(model, modelData.labels);
         }
 
-        async void Update () {
+        void Update () {
             // Check that the model has been downloaded
             if (predictor == null)
                 return;
-            // Check if the predictor is ready
-            if (!predictor.readyForPrediction)
-                return;
+            // Update pixel buffer to save on allocating memory
+            previewTexture.GetRawTextureData<byte>().CopyTo(pixelBuffer);
+            // Create input feature
+            var inputFeature = new MLImageFeature(pixelBuffer, previewTexture.width, previewTexture.height);
+            (inputFeature.mean, inputFeature.std) = modelData.normalization;
+            inputFeature.aspectMode = modelData.aspectMode;
             // Predict
-            var input = new MLImageFeature(previewTexture);
-            (input.mean, input.std) = modelData.normalization;
-            input.aspectMode = modelData.aspectMode;
-            var detections = await predictor.Predict(input);
+            var detections = predictor.Predict(inputFeature);
             // Visualize
             visualizer.Render(previewTexture, detections);
         }
 
         void OnDisable () {
             // Dispose the predictor and model
-            predictor?.Dispose();
             model?.Dispose();
             // Stop the camera preview
             if (cameraDevice?.running ?? false)
--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 ![cows](.media/cows.png)
 
-Realtime object detection in Unity Engine with NatML. This project uses the [@natsuite/tiny-yolo-v3](https://hub.natsuite.io/predictor/@natsuite/tiny-yolo-v3) package from NatML Hub.
+Realtime object detection in Unity Engine with NatML. This project uses the [@natsuite/ssd-lite](https://hub.natsuite.io/predictor/@natsuite/ssd-lite) package from NatML Hub.
 
 ## Requirements
 - Unity 2019.2+
-- NatML 1.0.0+
-- [@natsuite/tiny-yolo-v3](https://hub.natsuite.io/predictor/@natsuite/tiny-yolo-v3)
+- [NatML 1.0.4+](https://github.com/natsuite/NatML)
 - [NatDevice 1.1+](https://assetstore.unity.com/packages/tools/integration/natdevice-media-device-api-162053)
-- [NatShare 1.2+](https://github.com/natsuite/NatShare)
+- [@natsuite/ssd-lite](https://hub.natsuite.io/predictor/@natsuite/ssd-lite)
 
 ## Supported Platforms
 - Android API level 24+ (Android Nougat or newer)
@@ -18,7 +17,7 @@ Realtime object detection in Unity Engine with NatML. This project uses the [@na
 - Windows 10 (64-bit)
 
 ## Quick Tips
-- [Get NatML](https://docs.natsuite.io/natml/#get-natml).
+- [Get NatML](https://github.com/natsuite/NatML).
 - Join the [NatSuite community on Discord](https://discord.gg/y5vwgXkz2f).
 - Discuss [NatML on Unity Forums](https://forum.unity.com/threads/open-beta-natml-machine-learning-runtime.1109339/).
 - Contact us at [hi@natsuite.io](mailto:hi@natsuite.io).
